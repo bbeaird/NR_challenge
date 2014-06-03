752ca1e7113116564395b3de51fb479a94319ae2
Brantley Beaird <bbeaird@gmail.com>
2014-06-03 16:56:16 -0700

    Added get route that uses Mandrill...not working completely, but it recognizes the Madrill api key. Also created a gitignore to ignore .env.

--- email.rb
+++ email.rb
@@ -1,13 +1,9 @@
 require 'rubygems'
 require 'sinatra'
+require 'mandrill'
 
 get '/' do
   "Hello World!"
-end
-
-post '/send-email' do
-  # "You sent !!!!!!!!!!!!!!!!!!!!"
-  "You sent #{params}!!!!!!!!!!!!!!!!"
 end
 
 get '/form' do
@@ -15,5 +11,16 @@ get '/form' do
 end
 
 post '/form' do
+  "You sent #{params}!!!!!!!!!!!!!!!!"
+end
+
+get '/send-email' do
+  m = Mandrill::API.new
+  rendered = m.templates.render 'MyTemplate', [{:name => 'main', :content => 'The main content block'}]
+  puts rendered['html'] # print out the rendered HTML
+end
+
+post '/send-email' do
+  # "You sent !!!!!!!!!!!!!!!!!!!!"
   "You sent #{params}!!!!!!!!!!!!!!!!"
 end
