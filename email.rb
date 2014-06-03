48cda59c2bcc110ed0f78684a894b5afe65d6642
Brantley Beaird <bbeaird@gmail.com>
2014-06-03 16:24:53 -0700

    Created a form that shows the message when submitted.

--- email.rb
+++ email.rb
@@ -4,3 +4,16 @@ require 'sinatra'
 get '/' do
   "Hello World!"
 end
+
+post '/send-email' do
+  # "You sent !!!!!!!!!!!!!!!!!!!!"
+  "You sent #{params}!!!!!!!!!!!!!!!!"
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You sent #{params}!!!!!!!!!!!!!!!!"
+end
