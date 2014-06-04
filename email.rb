0b1bf666628da1686c1f76ce4abc65d6587de941
Brantley Beaird <bbeaird@gmail.com>
2014-06-04 16:35:20 -0700

    Successfully sent an email via smtp with gmail.

--- email.rb
+++ email.rb
@@ -1,8 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'mandrill'
+require 'action_mailer'
+require 'pony'
+require 'dotenv'
 
-# Not sure why I had to replace the colons with hashrockets???
+Dotenv.load
+
+class MyMailer < ActionMailer::Base
+  default from: 'noreply@example.com'
+
+  def test_email
+    mail(to: 'bbeaird@gmail.com', subject: 'Test subject doode!!!')
+  end
+end
+
+# Not sure why I had to replace the colons with hashrockets??? Ahhh...it's because the keys are actually strings instead of symbols!!!
 payload = { "to"=> "Natasha <natasha@newrelic.com>", "subject"=> "hello world", "body"=> "Hi Natasha! Sending you an email via this API I just made." }
 
 get '/' do
@@ -18,55 +31,24 @@ post '/form' do
 end
 
 get '/send-email' do
-  mandrill = Mandrill::API.new ENV['MANDRILL_API_KEY']
-  # result = mandrill.users.info
-  message = {"html"=>"<p>Example HTML content. Ahhhhhhhhhhhhhhhh!!!!!!!!</p>",
-     "text"=>"Example text content. Hello world!!!!!",
-     "subject"=>"example subject dude!!!",
-     "from_email"=>"app25992133@heroku.com",
-     "from_name"=>"Mandrill BIATCH!",
-     "to"=>
-        [{"email"=>"bbeaird@gmail.com",
-            "name"=>"Brantley Beaird",
-            "type"=>"to"}],
-     "headers"=>{"Reply-To"=>"app25992133@heroku.com"},
-     "important"=>false,
-     "track_opens"=>nil,
-     "track_clicks"=>nil,
-     "auto_text"=>nil,
-     "auto_html"=>nil,
-     "inline_css"=>nil,
-     "url_strip_qs"=>nil,
-     "preserve_recipients"=>nil,
-     "view_content_link"=>nil,
-     "bcc_address"=>"message.bcc_address@example.com",
-     "tracking_domain"=>nil,
-     "signing_domain"=>nil,
-     "return_path_domain"=>nil,
-     "merge"=>true,
-     "global_merge_vars"=>[{"name"=>"merge1", "content"=>"merge1 content"}],
-     "merge_vars"=>
-        [{"rcpt"=>"recipient.email@example.com",
-            "vars"=>[{"name"=>"merge2", "content"=>"merge2 content"}]}],
-     "tags"=>["password-resets"],
-     "subaccount"=>"customer-123",
-     "google_analytics_domains"=>["example.com"],
-     "google_analytics_campaign"=>"message.from_email@example.com",
-     "metadata"=>{"website"=>"www.example.com"},
-     "recipient_metadata"=>
-        [{"rcpt"=>"recipient.email@example.com", "values"=>{"user_id"=>123456}}],
-     "attachments"=>
-        [{"type"=>"text/plain",
-            "name"=>"myfile.txt",
-            "content"=>"ZXhhbXBsZSBmaWxl"}],
-     "images"=>
-        [{"type"=>"image/png", "name"=>"IMAGECID", "content"=>"ZXhhbXBsZSBmaWxl"}]}
-    async = false
-    ip_pool = "Main Pool"
-    # send_at = "2010-01-01 01:00:00"
-    send_at = Time.now
-    result = mandrill.messages.send message, async, ip_pool
-    "Done bitch!"
+  # MyMailer.test_email.deliver
+  # "Check your email doode!!!"
+  Pony.mail to: 'bbeaird@gmail.com',
+            via: 'smtp',
+            via_options: {
+              address: 'smtp.gmail.com',
+              port: '587',
+              enable_start_tls_auto: true,
+              user_name: 'bbeaird@gmail.com',
+              password: ENV['GMAIL_PASSWORD'],
+              authentication: :plain,
+              domain: 'localhost.localdomain'
+            },
+            # from: 'bbeaird@gmail.com',
+            subject: 'Howdy partner!',
+            body: 'Big plans to celebrate after you get this job?!'
+
+  'Check your email! Hopefully you received the above message!'
 end
 
 post '/send-email' do
