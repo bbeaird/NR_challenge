cb0dce342f9964bf5c056a6cbe8cc9dfb3b1680f
Brantley Beaird <bbeaird@gmail.com>
2014-06-04 21:03:58 -0700

    Cleaning things up.

--- email.rb
+++ email.rb
@@ -1,8 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 require 'mandrill'
-require 'action_mailer'
-require 'pony'
 require 'dotenv'
 require 'mail'
 
@@ -10,15 +8,7 @@ Dotenv.load
 
 # payload = { "to"=> "Natasha <natasha@newrelic.com>", "subject"=> "hello world", "body"=> "Hi Natasha! Sending you an email via this API I just made." }
 
-get '/form' do
-  erb :form
-end
-
-post '/form' do
-  "You sent #{params}!!!!!!!!!!!!!!!!"
-end
-
-payload = { "to"=> "Brantley <bbeaird@gmail.com>", "subject"=> "hello world", "body"=> "Hi Natasha! Sending you an email via this API I just made." }
+payload = { "to"=> "Brantley Beaird <bbeaird@gmail.com>", "subject"=> "hello world", "body"=> "Hi Natasha! Sending you an email via this API I just made." }
 
 get '/send-email' do
 
@@ -42,8 +32,4 @@ get '/send-email' do
   end
 
   'Check your email! Hopefully you received the above message!'
-end
-
-post '/send-email' do
-  "You sent #{params}!!!!!!!!!!!!!!!!"
 end
