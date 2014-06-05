fe7f05dbe27b91e20fa227dbfb18756d5d9b20f7
Brantley Beaird <bbeaird@gmail.com>
2014-06-04 21:00:46 -0700

    Can now send emails with data in hash.

--- email.rb
+++ email.rb
@@ -8,16 +8,7 @@ require 'mail'
 
 Dotenv.load
 
-# class MyMailer < ActionMailer::Base
-#   default from: 'noreply@example.com'
-
-#   def test_email
-#     mail(to: 'bbeaird@gmail.com', subject: 'Test subject doode!!!', body: 'Sample body text!!!')
-#   end
-# end
-
-# Not sure why I had to replace the colons with hashrockets??? Ahhh...it's because the keys are actually strings instead of symbols!!!
-payload = { "to"=> "Natasha <natasha@newrelic.com>", "subject"=> "hello world", "body"=> "Hi Natasha! Sending you an email via this API I just made." }
+# payload = { "to"=> "Natasha <natasha@newrelic.com>", "subject"=> "hello world", "body"=> "Hi Natasha! Sending you an email via this API I just made." }
 
 get '/form' do
   erb :form
@@ -27,23 +18,9 @@ post '/form' do
   "You sent #{params}!!!!!!!!!!!!!!!!"
 end
 
-get '/send-email' do
-  # MyMailer.test_email.deliver
-  # "Check your email doode!!!"
+payload = { "to"=> "Brantley <bbeaird@gmail.com>", "subject"=> "hello world", "body"=> "Hi Natasha! Sending you an email via this API I just made." }
 
-  # Pony.mail to: 'bbeaird@gmail.com',
-  #           via: 'smtp',
-  #           via_options: {
-  #             address: 'smtp.gmail.com',
-  #             port: '587',
-  #             enable_start_tls_auto: true,
-  #             user_name: 'bbeaird',
-  #             password: ENV['GMAIL_PASSWORD'],
-  #             authentication: :plain,
-  #             domain: 'localhost.localdomain'
-  #           },
-  #           subject: 'Howdy partner!',
-  #           body: 'Big plans to celebrate after you get this job?!'
+get '/send-email' do
 
   Mail.defaults do
     delivery_method :smtp, {
@@ -55,17 +32,12 @@ get '/send-email' do
   end
 
   mail = Mail.deliver do
-    to 'bbeaird@gmail.com'
-    from 'Brantley Beaird <noreply@example.com>'
-    subject 'Test subject doode!!!'
+    to payload["to"]
+    from '<noreply@example.com>'
+    subject payload["subject"]
 
     text_part do
-      body 'Mandrill speaks plaintext'
-    end
-
-    html_part do
-      content_type 'text/html; charset=UTF-8'
-      body '<em>Mandrill speaks <strong>HTML</strong></em>'
+      body payload["body"]
     end
   end
 
